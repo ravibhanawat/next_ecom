9e68372bae61b90e026f51ade63da72d3845c603
ravibhanawat <rbhanawat328@gmail.com>
2023-10-03 09:51:35 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import GlobalState from '@/context'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <GlobalState>
           <Navbar/>
           <main className='flex min-h-screen flex-col mt-[80px]'>{children}</main>
+          <Footer />
         </GlobalState>
       </body>
     </html>
